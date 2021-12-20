6eb450608cb77d157c920746321d64f0feb294b0
Jake Cleary <shout@jakecleary.net>
2021-12-20 19:20:44 +0000

    Errors: Add project to hold error related code; define some errors

---
+++ Jennie.Errors/Error.cs
@@ -0,0 +1,10 @@
+﻿namespace Jennie.Errors;
+
+public sealed record Error(int Code, string Description)
+{
+    public static Error InvalidCommand() => new(1, "No command given");
+    public static Error InvalidCommand(string value) => new(2, $"Invalid command '{value}' given");
+    public static Error ConfigFileNotFound() => new(3, "No jennie.config file was found in current directory");
+
+    public override string ToString() => $"Error: {Description} ({Code})";
+}
