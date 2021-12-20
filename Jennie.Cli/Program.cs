39801f1f0b828ddc2869155607e622165a93c52f
Jake Cleary <shout@jakecleary.net>
2021-12-20 19:21:24 +0000

    Cli: Add initial bootstrap to show contents of some config file

--- Jennie.Cli/Program.cs
+++ Jennie.Cli/Program.cs
@@ -1,3 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Jennie.Errors;
 
-Console.WriteLine("Hello, World!");
+if (!args.Any())
+{
+    var error = Error.InvalidCommand();
+    Console.WriteLine(error);
+    return error.Code;
+}
+
+var command = args[0];
+
+if (command != "build")
+{
+    var error = Error.InvalidCommand(command);
+    Console.WriteLine(error);
+    return error.Code;
+}
+
+const string configFileName = "jennie.config";
+
+if (!File.Exists(configFileName))
+{
+    var error = Error.ConfigFileNotFound();
+    Console.WriteLine(error);
+    return error.Code;
+}
+
+var configLines = await File.ReadAllLinesAsync(configFileName);
+
+Console.WriteLine($"{configFileName} contains the following:");
+Console.WriteLine();
+
+foreach (var configLine in configLines)
+{
+    Console.WriteLine(configLine);
+}
+
+return 0;
