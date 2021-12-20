8752953c6271a51f43f963099feda041882bbd0c
Jake Cleary <shout@jakecleary.net>
2021-12-20 19:43:44 +0000

    Cli: Parse the config file as key-value-pairs and output them (or any errors)

--- Jennie.Cli/Program.cs
+++ Jennie.Cli/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Jennie.Errors;
+using MoreLinq;
 
 if (!args.Any())
 {
@@ -30,9 +31,32 @@ var configLines = await File.ReadAllLinesAsync(configFileName);
 Console.WriteLine($"{configFileName} contains the following:");
 Console.WriteLine();
 
+var errors = new List<Error>();
+var configValues = new Dictionary<string, string>();
+
 foreach (var configLine in configLines)
 {
-    Console.WriteLine(configLine);
+    var parts = configLine.Split(": ");
+
+    if (parts.Length != 2)
+    {
+        errors.Add(Error.InvalidConfigLine(configLine));
+    }
+    else
+    {
+        var key = parts[0].Trim();
+        var value = parts[1].Trim(' ', '"');
+        configValues.Add(key, value);
+    }
 }
+
+if (errors.Any())
+{
+    errors.ForEach(Console.WriteLine);
+
+    return errors.First().Code;
+}
+
+configValues.ForEach(kvp => Console.WriteLine($"{kvp.Key}: {kvp.Value}"));
 
 return 0;
--- Jennie.Errors/Error.cs
+++ Jennie.Errors/Error.cs
@@ -5,6 +5,7 @@ public sealed record Error(int Code, string Description)
     public static Error InvalidCommand() => new(1, "No command given");
     public static Error InvalidCommand(string value) => new(2, $"Invalid command '{value}' given");
     public static Error ConfigFileNotFound() => new(3, "No jennie.config file was found in current directory");
+    public static Error InvalidConfigLine(string configLine) => new(4, $"Invalid config line -> {configLine}");
 
     public override string ToString() => $"Error: {Description} ({Code})";
 }
